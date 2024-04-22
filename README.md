4f17ca6f32ac07058c7e8638500e93bf10380bc6
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-04-22 19:50:14 +0600

    server basic setup, intregrate mongodb done

---
+++ README.md
@@ -0,0 +1,3 @@
+## Espresso Emporium Server
+
+Milestone: 10, Espresso Emporium Server. Backend based project with (Node, Express, Mongodb).
---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.5chsr9x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get('/', (req, res) => {
+  res.send('Espresso Emporium server is running');
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const coffeeDatabase = client.db('coffeeDB');
+    const coffeeCollection = coffeeDatabase.collection('coffeeCollection');
+
+    app.post('/coffees', async (req, res) => {
+      const newCoffee = req.body;
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Espresso Emporium server listening on port ${port}`);
+});
